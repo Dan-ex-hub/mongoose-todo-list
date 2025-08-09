aa88af4bd150c612d09dc725a2fd90c9097d46f0
Code-with-Danie <danrose200@student.sfit.ac.in>
2025-08-09 12:02:04 +0530

    updated

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-
 const express = require("express");
+const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
+
 let app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-
+app.use(methodOverride("_method"));
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://danrose200:ipcGUoPi2LCBkGqA@test-todo-db.zifmtcq.mongodb.net/?retryWrites=true&w=majority&appName=test-todo-db");//database name todo
+const MONGO = process.env.MONGO || "mongodb+srv://danrose200:ipcGUoPi2LCBkGqA@test-todo-db.zifmtcq.mongodb.net/?retryWrites=true&w=majority&appName=test-todo-db";
+mongoose.connect(MONGO);
+
 const trySchema = new mongoose.Schema({
     name: String,
     priority: {
         type: String,
         enum: ["High", "Mid", "Low"],
-        default: "Mid"
     }
 });
-const item = mongoose.model("task", trySchema) //collection,SchemaName
-
-
+const item = mongoose.model("tasks", trySchema);
 
 app.get("/", async function (req, res) {
     try {
@@ -29,68 +28,56 @@ app.get("/", async function (req, res) {
         if (priority) {
             filter.priority = priority;
         }
-        const foundItems = await item.find(filter);
-        res.render("list", { dayej: foundItems, priority: priority });
+        const founditem = await item.find(filter);
+        res.render("list", { dayej: founditem, priority: priority });
     } catch (err) {
         console.error("Find error:", err);
-        res.send("Error occurred while fetching data.");
     }
 });
 
-
-
 app.post("/", async function (req, res) {
-    const itemName = req.body.ele1;
-    const priority = req.body.priority || "Mid";
     const todo = new item({
-        name: itemName,
-        priority: priority
+        name: req.body.ele1,
+        priority: req.body.priority
     });
     try {
         await todo.save();
         res.redirect("/");
     } catch (err) {
-        console.error("Save error:", err);
-        res.send("Error occurred while saving data.");
+        res.send("Error occurred while creating item. " + err);
     }
 });
 
-
-
-app.post("/delete", async function (req, res) {
-    const checkedItemId = req.body.checkedbox;
+app.delete("/delete/:id", async function (req, res) {
     try {
-        await item.findByIdAndDelete(checkedItemId);
+        await item.findByIdAndDelete(req.params.id);
         res.redirect("/");
     } catch (err) {
-        console.error("Delete error:", err);
         res.send("Error occurred while deleting item. " + err);
     }
 });
 
-
 app.get("/edit/:id", async function (req, res) {
     try {
-        const foundItem = await item.findById(req.params.id);
-        res.render("edit", { item: foundItem });
+        const founditem = await item.findById(req.params.id);
+        res.render("edit", { item: founditem });
     } catch (err) {
         res.send("Error occurred while loading edit page. " + err);
     }
 });
 
-app.post("/edit/:id", async function (req, res) {
-    const itemId = req.body.itemId;
-    const updatedName = req.body.updateItems;
-    const updatedPriority = req.body.priority || "Mid";
+app.put("/edit/:id", async function (req, res) {
     try {
-        await item.findByIdAndUpdate(itemId, { name: updatedName, priority: updatedPriority });
+        await item.findByIdAndUpdate(req.params.id, { 
+            name: req.body.updateitems, 
+            priority: req.body.priority 
+        });
         res.redirect("/");
     } catch (err) {
         res.send("Error occurred while editing item. " + err);
     }
 });
 
-
-app.listen(3000, function () {
+app.listen(5000, function () {
     console.log("Server started");
 });
